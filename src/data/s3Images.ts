2d9fca13dbc8662bc122fb4adebe0803e597a0f3
waseemsamra <waseemsamra@gmail.com>
2026-03-26 17:18:17 +0400

    updatte

--- src/data/s3Images.ts
+++ src/data/s3Images.ts
@@ -6,34 +6,34 @@ export const S3_BASE_URL = `https://${S3_BUCKET}.s3.amazonaws.com`;
 
 export const HOMEPAGE_S3_IMAGES = {
   // Hero Section
-  heroMain: `${S3_BASE_URL}/hero/f6aa9714-0d85-4cff-88c2-aaa7247604a1-1774518226715.jpg`,
-  
+  heroMain: `${S3_BASE_URL}/hero/49ed6dd4-0a37-47c8-acf0-c1f3ba417e73-1774530690691.jpg`,
+
   // Portfolio Section
-  portfolioRiceSpices: `${S3_BASE_URL}/portfolio/cf6c1554-62a4-4494-862d-8577fb1e6447-1774518226717.jpg`,
-  portfolioCitrus: `${S3_BASE_URL}/portfolio/6be30d9f-7a3d-40d7-9215-19d1ce3f8c8c-1774518226719.jpg`,
-  portfolioGrains: `${S3_BASE_URL}/portfolio/28d41399-d429-475e-8a62-a2ebb1e139be-1774518228986.jpg`,
-  portfolioProduce: `${S3_BASE_URL}/portfolio/789103b7-1d2f-40ee-951d-3ce8d0486e95-1774518228988.jpg`,
-  
+  portfolioRiceSpices: `${S3_BASE_URL}/portfolio/8fe26692-fba8-4eb0-9df6-48c683948968-1774530690686.jpg`,
+  portfolioCitrus: `${S3_BASE_URL}/portfolio/15cdf0ec-69a9-4bd4-ae0f-99bf26d52ae9-1774530690679.jpg`,
+  portfolioGrains: `${S3_BASE_URL}/portfolio/8a48f0c5-c89f-4338-a60f-2114c926be11-1774530694535.jpg`,
+  portfolioProduce: `${S3_BASE_URL}/portfolio/009ab05c-3153-422e-9410-fa2aec40f982-1774530694541.jpg`,
+
   // Leadership Section
-  leadershipCeo: `${S3_BASE_URL}/leadership/c0b0a2d8-f24d-43b9-b150-af35d24f7d08-1774518228992.jpg`,
-  
+  leadershipCeo: `${S3_BASE_URL}/leadership/30799e60-aa34-454b-8f6f-b0833a55024c-1774530694532.jpg`,
+
   // Infrastructure Section
-  infrastructureLogistics: `${S3_BASE_URL}/infrastructure/0083ad2b-3b28-45bb-b9c5-d95ae79dd643-1774518229772.jpg`,
-  
+  infrastructureLogistics: `${S3_BASE_URL}/infrastructure/3b564844-9aa5-4b43-805a-3a8d1d3ef64c-1774530695612.jpg`,
+
   // Global Network Section
-  networkMap: `${S3_BASE_URL}/network/20b45e54-dee7-463d-87fd-16ea6878803f-1774518229780.jpg`,
-  
+  networkMap: `${S3_BASE_URL}/network/ab8208b3-2d50-4923-be33-2f42274653ac-1774530695615.jpg`,
+
   // CSR Section
-  csrSoilHands: `${S3_BASE_URL}/csr/f36390fa-c7dd-4e1d-8e35-48e5670b9a88-1774518229784.jpg`,
-  csrSolarPanels: `${S3_BASE_URL}/csr/b8151c1e-62fd-4878-ad4a-e510610ebcbf-1774518230471.jpg`,
-  
+  csrSoilHands: `${S3_BASE_URL}/csr/fa7f89d2-fbba-4385-a1bd-246ba63b45f4-1774530695621.jpg`,
+  csrSolarPanels: `${S3_BASE_URL}/csr/38bda804-05d9-4ad0-9ed2-e2152cb05188-1774530696649.jpg`,
+
   // News Section
-  newsGenevaMeeting: `${S3_BASE_URL}/news/497c60de-df71-4d34-b2ea-4d326be0e63e-1774518230477.jpg`,
-  newsAgriLab: `${S3_BASE_URL}/news/b288104e-8c58-40b1-a2ef-8fe325eb3061-1774518230480.jpg`,
-  newsCoffeeHandshake: `${S3_BASE_URL}/news/ad745345-9aa8-4731-866a-c184b353eec5-1774518231309.jpg`,
-  
+  newsGenevaMeeting: `${S3_BASE_URL}/news/7ed874ad-8059-496f-9e2f-edbbf77c57bd-1774530696653.jpg`,
+  newsAgriLab: `${S3_BASE_URL}/news/fa791451-7b31-4862-9dc1-ed7d8461ed94-1774530696656.jpg`,
+  newsCoffeeHandshake: `${S3_BASE_URL}/news/2cabf4a6-bfef-4cc4-9178-71c0437cb-1774530697714.jpg`,
+
   // CTA Section
-  ctaWheatPattern: `${S3_BASE_URL}/cta/8b0d7743-96cb-498e-b8d4-17a99060f630-1774518231312.jpg`
+  ctaWheatPattern: `${S3_BASE_URL}/cta/0ad6df9d-6dc1-4aed-9ed2-e2152cb05188-1774530697717.jpg`
 };
 
 export default HOMEPAGE_S3_IMAGES;
